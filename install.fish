9008a71d1b3d0a7b7a6073fcf7b2763db8e1c9ce
Lucifer Sears <lucifer.sears@gmail.com>
2019-10-06 21:14:13 -0400

    add basic configuration

---
+++ install.fish
@@ -0,0 +1,21 @@
+set fish_config $HOME/.config/fish/config.fish
+
+if ! test -f $fish_config
+  echo '
+set sugar_install_directory $HOME/.config/fish/sugar
+
+source $sugar_install_directory/sugar.fish
+
+set modules \
+  sugar-core:developer \
+  sugar-core:prompt \
+  sugar-core:git \
+  sugar-core:ssh \
+  sugar-core:tmux
+
+for module in $modules
+  sugar-load $module
+end
+  ' >> $fish_config
+  exec fish
+end
