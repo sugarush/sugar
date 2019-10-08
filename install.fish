ca37c624ccd598616b5c3bdee63002b0c1588f30
Lucifer Sears <lucifer.sears@gmail.com>
2019-10-08 10:29:06 -0400

    create modules and config directories

--- install.fish
+++ install.fish
@@ -1,5 +1,13 @@
 set fish_config $HOME/.config/fish/config.fish
 
+if ! test -d $HOME/.config/fish/sugar/modules
+  mkdir -p $HOME/.config/fish/sugar/modules
+end
+
+if ! test -d $HOME/.config/fish/sugar/config
+  mkdir -p $HOME/.config/fish/sugar/config
+end
+
 if ! test -f $fish_config
   echo '
 set sugar_install_directory $HOME/.config/fish/sugar
@@ -19,5 +27,6 @@ end
 
 sugar-on-load
 ' > $fish_config
+
   exec fish
 end
